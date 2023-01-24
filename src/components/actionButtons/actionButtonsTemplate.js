451b7badee831b3c12b56b16f87cd9e9e39065d9
Xadxdxd <xadxd1@gmail.com>
2023-01-24 20:05:18 +0100

    add button del. last comp. add logic del all/last

--- src/components/actionButtons/actionButtonsTemplate.js
+++ src/components/actionButtons/actionButtonsTemplate.js
@@ -3,6 +3,7 @@ export function actionButtonsTemplate() {
   <div class='flex direction-col'>
     <button id="button--delete-all">Delete all</button>
     <button id="button--delete-component">Delete component</button>
+    <button id="button--delete-last-component">Delete last component</button>
     <button id="button--download">Download</button>
   </div>
   `;
