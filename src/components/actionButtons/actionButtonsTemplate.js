1211b174e184a48954f0d2fd998fb1ffbafde9c7
Boi.zip <boirpm@outlook.com>
2023-01-26 14:41:04 +0100

    Fixes and added to the page

--- src/components/actionButtons/actionButtonsTemplate.js
+++ src/components/actionButtons/actionButtonsTemplate.js
@@ -2,8 +2,9 @@ export function actionButtonsTemplate() {
   return `
   <div class='flex direction-col'>
     <button id="button--delete-all">Delete all</button>
-    <button id="button--delete-component">Delete component</button>
+    <button id="button--delete-component" onclick='lookelem()'>Delete component</button>
     <button id="button--download">Download</button>
+    <button id="sortelem" onclick="sortelems()">Change components order</button>
   </div>
   `;
 }
