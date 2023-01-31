f02f273b06c265756d621d7119f6887620fe7a06
itgeniohakaton <hakaton.itgenio@gmail.com>
2023-01-31 01:41:37 +0100

    fix: refactoring

--- src/components/footer/footerUtils.js
+++ src/components/footer/footerUtils.js
@@ -1,7 +1,10 @@
 import { footerTemplate } from "./footerTemplates.js";
 
+const defaultImg =
+  "https://github.com/itgeniohakaton/site-constructor/blob/master/defaultLogoWhite.png?raw=true";
+
 export function addFooter(event, site) {
-  const imgLink = event.target.imgLink.value || "defaultLogoWhite.png";
+  const imgLink = event.target.imgLink.value || defaultImg;
   const name = event.target.name.value || "John Doe";
   const phone = event.target.phone.value || "+ 123 456 78 90";
   const background = event.target.background.value || "footerBackgroundBlue";
