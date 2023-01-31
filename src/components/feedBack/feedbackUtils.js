41acf245c4117bec660b83a530f402af5443c737
itgeniohakaton <hakaton.itgenio@gmail.com>
2023-01-31 08:36:27 +0100

    wip: 404

--- src/components/feedBack/feedbackUtils.js
+++ src/components/feedBack/feedbackUtils.js
@@ -1,4 +1,4 @@
-import { feedbackTemplate } from "feedbackTemplates.js";
+import { feedbackTemplate } from "./feedbackTemplates.js";
 export function addFeedback(event, site) {
   const background = event.target.background.value || "feedbackBackgroundBlue";
   const font = event.target.font.value || "feedbackFontNunito";
